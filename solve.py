02a5be0caf79f37d3240cc69661004c9621b39df
Chris Suran <suran.c@gmail.com>
2021-01-26 02:52:22 -0500

    Add carry branching on first node

--- solve.py
+++ solve.py
@@ -9,17 +9,30 @@ def populateSolutions(inputArray, solutions):
             solutions[letter] = 'null'
 
 def solveNode(index, input, sum, solutions, carry, carryForward):  #### Add carry, changing carryForward to if it pushes forward a 1 or not, and then have carry, whether or not it takes a carry?
+    # Create branch solutions to carry forward
+    branchSolutions = solutions.copy()
+    branchSolutionsCarry = solutions.copy()
+
     # Check base case - TODO Check for solution before we hit this point?
     if (index == len(input[0])):
         return solutions # Check if all letters != null, otherwise return unsolvable?
     # If solution at index of sum is null, and space is in input, letter must be 1.  Since carry forward at front can't be zero, and addition can only carry forward one
-    elif ( (solutions[sum[index]] == 'null') and (input[0] == ' ') ):
+    elif ( (solutions[sum[index]] == 'null') and (input[0][index] == ' ') ):
         solutions[sum[index]] = 1
+        
+        print ("Solved first letter")
+
+        # Solve with and without carry, return first result if solvable, and if not, whatever second result is (unsolvable still, or a valid solution)
+        result = solveNode(index+1, input, sum, branchSolutions, 0, 1)
+        if (result != 'unsolvable'):
+            return result
+        else:
+            return (solveNode(index+1, input, sum, branchSolutions, 1, 1))
     else:
         # Make set to iterate through
         # digit # = unsolve input1, input2, sum1
         # possibilities = numbers not found in solutions
-        return "solutions" # Solve problem for each carry possibility
+        return "unsolvable" # Solve problem for each carry possibility
     
     # noCarrySolutions = solutions.copy()
     # noCarry = solveNode(0, input, sum, noCarrySolutions, 0)
@@ -32,8 +45,6 @@ def solveNode(index, input, sum, solutions, carry, carryForward):  #### Add carr
     # if (withCarry != 'unsolvable'):
     #     print ("Solution Found!")
     #     print(withCarry)
-
-    return 'unsolvable'
 
 def solveProblem(input, sum):
     solutions = dict()
