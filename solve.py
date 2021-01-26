4c18999ca5867a71d581f7919db8a78311eaf4f4
Chris Suran <suran.c@gmail.com>
2021-01-26 01:21:14 -0500

    Add framework to solve problem and move solutions population into it

--- solve.py
+++ solve.py
@@ -3,13 +3,20 @@
 input = [["S", "E", "N", "D"], ["M", "O", "R", "E"]]
 sum = ["M", "O", "N", "E", "Y"]
 
-solutions = dict()
-
 def populateSolutions(inputArray, solutions):
     for letter in inputArray:
         solutions[letter] = 'null'
 
-populateSolutions(input[0], solutions)
-populateSolutions(input[1], solutions)
-populateSolutions(sum, solutions)
+def solveNode(index, solutions, carryForward):
+    print("todo")
 
+def solveProblem(input, sum):
+    solutions = dict()
+
+    populateSolutions(input[0], solutions)
+    populateSolutions(input[1], solutions)
+    populateSolutions(sum, solutions)
+
+    print(solutions)
+
+solveProblem(input, sum)
