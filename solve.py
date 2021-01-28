b7febe932687b3c007f6cd89dab7d8df4a14d94e
Chris Suran <suran.c@gmail.com>
2021-01-28 00:29:16 -0500

    Fix some branching errors, and cleanup comments

--- solve.py
+++ solve.py
@@ -10,7 +10,6 @@ def populateSolutions(inputArray, solutions):
 
 def populateTestCases(letter1, letter2, letter3, solutions):
     unsolvedNumbers = {'0': '0', '1': '0', '2': '0', '3': '0', '4': '0', '5': '0', '6': '0', '7': '0', '8': '0', '9': '0'}
-    #unsolvedNumbers = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
     result = []
     letters = [letter1, letter2, letter3]
 
@@ -39,55 +38,52 @@ def solveNode(index, input, sum, solutions, carry, carryForward):  #### Add carr
 
     # Check base case - TODO Check for solution before we hit this point?
     if (index == len(input[0])):
-        print("all your base")
         return solutions # Check if all letters != null, otherwise return unsolvable?
+
     # If solution at index of sum is null, and space is in input, letter must be 1.  Since carry forward at front can't be zero, and addition can only carry forward one
     elif ( (solutions[sum[index]] == 'null') and (input[0][index] == ' ') ):
         branchSolutions[sum[index]] = '1'
         branchSolutionsCarry[sum[index]] = '1'
-        
-        print ("Solved first letter")
 
         # Solve with and without carry, return first result if solvable, and if not, whatever second result is (unsolvable still, or a valid solution)
         result = solveNode(index+1, input, sum, branchSolutions, 0, 1)
         if (result != 'unsolvable'):
             return result
-        else:
-            return (solveNode(index+1, input, sum, branchSolutionsCarry, 1, 1))
+        carryResult = solveNode(index+1, input, sum, branchSolutionsCarry, 1, 1)
+        if (carryResult != 'unsolvable'):
+            return (carryResult)
+        return "unsolvable"
     else:
         # Get test cases from three digits, no carry first
         testCases = populateTestCases(input[0][index], input[1][index], sum[index], branchSolutions)
-        print (testCases)
+
         # Loop through each layer of the test cases, testing each permutation
         for input1 in testCases[0]:
             for input2 in testCases[1]:
                 for sum3 in testCases[2]:
                     # Skip cases where candidate is already found (shouldn't happen, FIX-skip for now) or duplicate candidates are hit
                     if ( (input1 == input2) or (input1 == sum3) or (input1 == sum3) or (input2 == sum3) ):
-                        print("skipping...")
-                        print ("carryForward: " + str(carryForward) + " input1: " + input1 + " - input2: " + input2 + " - carry: " + str(carry) + " - sum3: " + str(int(sum3) + 10*carryForward))
                         next
 
-                    #if (carryForward == 1): # If carryFoward = 1, sum must be >= 10, if 0, must be < 10
-                    if (int(input1) + int(input2) + int(carry) == int(sum3) + 10*carryForward):
+                    # If carryFoward = 1, sum must be >= 10, if 0, must be < 10
+                    if (int(input1) + int(input2) + int(carry) == (int(sum3) + 10*carryForward)):
                         # Populate these test solutions into branchSolutions
                         branchSolutions[input[0][index]] = input1
                         branchSolutions[input[1][index]] = input2
                         branchSolutions[sum[index]] = sum3
+                        
+                        # Solve with and without carry, return first result if solvable, and if not, whatever second result is (unsolvable still, or a valid solution)
+                        result = solveNode(index+1, input, sum, branchSolutions, 0, carry)
+                        if (result != 'unsolvable'):
+                            return result
+
                         branchSolutionsCarry[input[0][index]] = input1
                         branchSolutionsCarry[input[1][index]] = input2
                         branchSolutionsCarry[sum[index]] = sum3
-                        print ("carryForward: " + str(carryForward) + " input1: " + input1 + " - input2: " + input2 + " - carry: " + str(carry) + " - sum3: " + str(int(sum3) + 10*carryForward))
-                        # Solve with and without carry, return first result if solvable, and if not, whatever second result is (unsolvable still, or a valid solution)
-                        result = solveNode(index+1, input, sum, branchSolutions, 0, carry)
-                        print (index+1, input, sum, branchSolutions, 0, carry)
+                        carryResult = solveNode(index+1, input, sum, branchSolutionsCarry, 1, carry)
                         if (result != 'unsolvable'):
-                            print ("not unsolve")
-                            print (result)
-                            return result
-                        else:
-                            print("returning from carry")
-                            return (solveNode(index+1, input, sum, branchSolutionsCarry, 1, carry))
+                            return (carryResult)
+
     return "unsolvable"
 
 def solveProblem(input, sum):
