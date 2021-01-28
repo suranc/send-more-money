18e57a5d43ebba99259a397c0882192d5449f3a7
Chris Suran <suran.c@gmail.com>
2021-01-28 01:00:08 -0500

    Clean up some logic, and move copy to inside loop to prevent reusing same reference

--- solve.py
+++ solve.py
@@ -32,37 +32,40 @@ def populateTestCases(letter1, letter2, letter3, solutions):
     return result
 
 def solveNode(index, input, sum, solutions, carry, carryForward):  #### Add carry, changing carryForward to if it pushes forward a 1 or not, and then have carry, whether or not it takes a carry?
-    # Create branch solutions to carry forward
-    branchSolutions = solutions.copy()
-    branchSolutionsCarry = solutions.copy()
-
     # Check base case - TODO Check for solution before we hit this point?
     if (index == len(input[0])):
         return solutions # Check if all letters != null, otherwise return unsolvable?
 
     # If solution at index of sum is null, and space is in input, letter must be 1.  Since carry forward at front can't be zero, and addition can only carry forward one
     elif ( (solutions[sum[index]] == 'null') and (input[0][index] == ' ') ):
+        # Create branch solutions to carry forward
+        branchSolutions = solutions.copy()
         branchSolutions[sum[index]] = '1'
-        branchSolutionsCarry[sum[index]] = '1'
 
         # Solve with and without carry, return first result if solvable, and if not, whatever second result is (unsolvable still, or a valid solution)
         result = solveNode(index+1, input, sum, branchSolutions, 0, 1)
         if (result != 'unsolvable'):
             return result
+
+        branchSolutionsCarry = solutions.copy()
+        branchSolutionsCarry[sum[index]] = '1'
         carryResult = solveNode(index+1, input, sum, branchSolutionsCarry, 1, 1)
         if (carryResult != 'unsolvable'):
             return (carryResult)
         return "unsolvable"
     else:
         # Get test cases from three digits, no carry first
-        testCases = populateTestCases(input[0][index], input[1][index], sum[index], branchSolutions)
+        testCases = populateTestCases(input[0][index], input[1][index], sum[index], solutions)
 
         # Loop through each layer of the test cases, testing each permutation
         for input1 in testCases[0]:
             for input2 in testCases[1]:
                 for sum3 in testCases[2]:
+                    # Create branch solutions to carry forward
+                    branchSolutions = solutions.copy()
+
                     # Skip cases where candidate is already found (shouldn't happen, FIX-skip for now) or duplicate candidates are hit
-                    if ( (input1 == input2) or (input1 == sum3) or (input1 == sum3) or (input2 == sum3) ):
+                    if ( (input1 == input2) or (input1 == sum3) or (input2 == sum3) ):
                         next
 
                     # If carryFoward = 1, sum must be >= 10, if 0, must be < 10
@@ -77,11 +80,12 @@ def solveNode(index, input, sum, solutions, carry, carryForward):  #### Add carr
                         if (result != 'unsolvable'):
                             return result
 
+                        branchSolutionsCarry = solutions.copy()
                         branchSolutionsCarry[input[0][index]] = input1
                         branchSolutionsCarry[input[1][index]] = input2
                         branchSolutionsCarry[sum[index]] = sum3
                         carryResult = solveNode(index+1, input, sum, branchSolutionsCarry, 1, carry)
-                        if (result != 'unsolvable'):
+                        if (carryResult != 'unsolvable'):
                             return (carryResult)
 
     return "unsolvable"
